9193aaea92cd3d365a851d8c49befa62e388225c
zunzunwang <zunzunwang@gmail.com>
2017-01-06 05:07:50 +0100

    ajouter junit
    ajouter junit

---
+++ testEAN/src/test/java/org/zzw/testEAN/EanLookUpImplTest.java
@@ -0,0 +1,24 @@
+package org.zzw.testEAN;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EanLookUpImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testReadFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetResult() {
+		fail("Not yet implemented");
+	}
+
+}
